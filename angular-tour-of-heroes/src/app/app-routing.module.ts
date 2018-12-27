f303866eb7fbfea39c9ddfeb229eeae25b7e4613
Paul Robert Martinez <paul.r.o.martinez@accenture.com>
2018-12-27 15:41:31 +0800

    added route on /heroes

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent }      from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
