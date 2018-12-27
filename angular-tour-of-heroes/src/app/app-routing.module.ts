aeff2b48284d3fc7620a0e2b079be93f9d96f77a
Paul Robert Martinez <paul.r.o.martinez@accenture.com>
2018-12-27 16:10:51 +0800

    added root navigation and deep link

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent }      from './heroes/heroes.component';
-
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
